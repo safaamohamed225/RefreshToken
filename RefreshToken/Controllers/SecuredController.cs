b1107535871916f8b6ac0ec5f203a9dcb06e92d7
Safa Mohamed <safaa.m.ibrahem@gmail.com>
2025-10-15 15:35:32 +0300

    GetSecuredData

---
+++ RefreshToken/Controllers/SecuredController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RefreshToken.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class SecuredController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult GetSecuredData()
+        {
+            var userName = User.Identity?.Name;
+            return Ok(new { Message = $"Hello, {userName}. This is secured data." });
+        }
+    }
+}
